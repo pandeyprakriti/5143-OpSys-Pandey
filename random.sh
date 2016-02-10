0eeac7fc7b3f00edd4e888ad22a95adc6a92e893
pandey <pandeyp@cs2.mwsu.edu>
2016-02-10 12:29:48 -0600

    added

--- random.sh
+++ random.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+function rndword {
+  n=/usr/share/dict/words
+
+  words=$(wc -l $n | awk '{print $1}')
+
+  rlno=$(($(cat /dev/random | od -N3 -An -D) % $words))
+  awk "NR == $rlno" $n
+#echo $dictionary
+}
+
+rndword
